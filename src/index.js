b3ffb55b5d26457b1675c16412ab6fbf2dab1030
Sunaked <syrgulev-aa@mail.ru>
2022-07-25 22:12:26 +0300

    delete comments

--- src/index.js
+++ src/index.js
@@ -40,54 +40,5 @@ function App() {
     </div>
   );
 }
-
-//class Page extends React.Component {
-//let caolor = useState("#000000");
-
-//function ChangeColor() {
-
-//	setColor(randomColor())
-//}
-
-//constructor(props) {
-//	super(props);
-//this.myRef = React.createRef();
-//this.focusMyRef = this.focusMyRef.bind(this);
-//	}
-//	setRef = (ref) => {
-//this.ref = ref;
-//};
-
-//componentDidMount() {
-//console.log(this.ref); // div
-//}
-
-//focusMyRef() //{
-//this.myRef.current.focus();
-//}
-/*
-	renderButton() {
-		const color = randomColor();
-		return (<Button value={color}/>);
-	}	
-	ChangeColor() {
-		console.log("CLICK");
-		console.log("color " + randomColor());
-		this.color = randomColor();
-		this.focusBackColor.style.backgroundColor = this.color; //this is how background will be updated
-	}
-	render() {
-		return(
-			<div className="page"  style={{backgroundColor:{color}, padding:'10px'}}> 
-				<div className="biggestPart" style={{width:'100%', height:'100%'}}>
-				<button className="button" onClick={ChangeColor}>Click Me! </button>
-			</div>
-			</div>
-		);
-	}
-	*/
-//}
-// ========================================
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
